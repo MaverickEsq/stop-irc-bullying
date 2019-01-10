506e9eeac7afa35546b9495c8e782d9065f379db
MaverickEsq <kemosabe@kemosabe.in>
2019-01-10 09:04:25 +0000

    Initial Upload

---
+++ files/anti-bully.mrc
@@ -0,0 +1,17 @@
+alias bully {
+  var %m I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop
+  if ($1) {
+    var %i 1
+    while (%i <= $0) {
+      say $($ $+ %i,2) $+ : %m
+      inc %i
+    }
+  }
+  else {
+    say $active $+ : %m
+  }
+}
+
+menu nicklist {
+  .bully-warn $snicks:bully $replace($snicks,$chr(44),$chr(32))
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$page = rtrim($_GET["c"], '/');
+
+require('./inc/header.inc');
+switch ($page) {
+	case "about":
+	default:
+		require('./inc/content/about.inc');
+		break;
+	case "what-is-bullying":
+		require('./inc/content/what-is.inc');
+		break;
+	case "fighting-bullying":
+		require('./inc/content/fighting.inc');
+		break;
+	case "live-chat":
+		require('./inc/content/chat.inc');
+		break;
+	case "stop":
+		require('./inc/content/stop.inc');
+		break;
+	case "contact":
+		require('./inc/content/contact.inc');
+		break;
+	case "fighting-bullying/helpful-tips":
+	case "helpful-tips":
+		require('./inc/content/tips.inc');
+		break;
+	case "fighting-bullying/anti-bully-alias":
+		require('./inc/content/alias.inc');
+		break;
+	case "fighting-bullying/best-practice":
+	case "best-practice":
+		require('./inc/content/best-prac.inc');
+		break;
+}
+require('./inc/footer.inc');
+?>
